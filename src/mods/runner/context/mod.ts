4a476e26a29e94a3a28b7850efdf4db77b441600
hazae41 <4405263+hazae41@users.noreply.github.com>
2025-09-24 11:09:42 +0700

    d

--- src/mods/runner/context/mod.ts
+++ src/mods/runner/context/mod.ts
@@ -1,3 +1,4 @@
+import Node from "node:test";
 import type { Closure } from "../closure/mod.ts";
 import { TestError } from "../error/mod.ts";
 
@@ -5,7 +6,7 @@ export interface Context {
 
   readonly name: string
 
-  test(name: string, closure: Closure): Promise<boolean>
+  test(name: string, closure: Closure): Promise<void>
 
 }
 
@@ -15,7 +16,7 @@ export namespace Context {
     if ("Deno" in globalThis)
       return DenoContext.test(name, closure)
     if ("process" in globalThis)
-      return Standalone.test(name, closure)
+      return NodeContext.test(name, closure)
     return Standalone.test(name, closure)
   }
 
@@ -37,13 +38,13 @@ export namespace Context {
       }).catch(console.error)
     }
 
-    async test(name: string, closure: Closure): Promise<boolean> {
+    async test(name: string, closure: Closure): Promise<void> {
       try {
         const subcontext = new Standalone(name)
 
         await closure(subcontext)
 
-        return true
+        return
       } catch (cause: unknown) {
         throw new TestError(name, { cause })
       }
@@ -73,8 +74,30 @@ export namespace Context {
      * @param closure closure to run
      * @returns result of closure
      */
-    test(name: string, closure: Closure): Promise<boolean> {
-      return this.inner.step(name, c => Promise.try(() => closure(new DenoContext(c))))
+    async test(name: string, closure: Closure): Promise<void> {
+      return void await this.inner.step(name, c => Promise.try(() => closure(new DenoContext(c))))
+    }
+
+  }
+
+  export class NodeContext implements Context {
+
+    constructor(
+      readonly inner: Node.TestContext
+    ) {
+      this.test = this.test.bind(this)
+    }
+
+    static test(name: string, closure: Closure) {
+      Node.test(name, t => Promise.try(() => closure(new NodeContext(t))))
+    }
+
+    get name(): string {
+      return this.inner.name
+    }
+
+    test(name: string, closure: Closure): Promise<void> {
+      return this.inner.test(name, t => Promise.try(() => closure(new NodeContext(t))))
     }
 
   }
